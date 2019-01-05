759af20b10b6a53d92c80a7b8e6abd50208435a5
muskan09 <muskankalsi9@gmail.com>
2019-01-06 01:30:00 +0530

    adds pep8

--- finaldatabase_setup.py
+++ finaldatabase_setup.py
@@ -2,21 +2,22 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
- 
+
 Base = declarative_base()
 
+
 class User(Base):
-	__tablename__ = 'user'
-	
-	id = Column(Integer, primary_key=True)
-	name = Column(String(250), nullable=False)
-	email = Column(String(250), nullable=False)
-	picture = Column(String(250))
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
 
 
 class University(Base):
     __tablename__ = 'university'
-   
+
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     user_id = Column(Integer, ForeignKey('user.id'))
@@ -24,41 +25,38 @@ class University(Base):
 
     @property
     def serialize(self):
-       """Return object data in easily serializeable format"""
-       return {
-           'name'         : self.name,
-           'id'           : self.id,
-       }
- 
+        """Return object data in easily serializeable format"""
+        return {
+           'name': self.name,
+           'id': self.id,
+        }
+
+
 class Course(Base):
     __tablename__ = 'course'
 
-
-    name =Column(String(250), nullable = False)
-    id = Column(Integer, primary_key = True)
+    name = Column(String(250), nullable=False)
+    id = Column(Integer, primary_key=True)
     description = Column(String(250))
     professor = Column(String(250))
     school = Column(String(250))
-    university_id = Column(Integer,ForeignKey('university.id'))
+    university_id = Column(Integer, ForeignKey('university.id'))
     university = relationship(University)
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
 
-
     @property
     def serialize(self):
-       """Return object data in easily serializeable format"""
-       return {
-           'name'         : self.name,
-           'description'         : self.description,
-           'id'         : self.id,
-           'professor'         : self.professor,
-           'school'         : self.school,
-       }
-
+        """Return object data in easily serializeable format"""
+        return {
+           'name': self.name,
+           'description': self.description,
+           'id': self.id,
+           'professor': self.professor,
+           'school': self.school,
+        }
 
 
 engine = create_engine('sqlite:///curriculum.db')
- 
 
 Base.metadata.create_all(engine)
